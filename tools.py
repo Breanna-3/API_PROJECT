cdc61017b7a21dbe0146c0c7477ac590e9ca0373
dj3lq <a.rodic0220241@arts.ac.uk>
2025-05-27 22:56:48 +0100

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sqlite3
 
+# Helper function to run SQL quert and return a DataFrame
 def df_from_query(table, query, params={}):
     try:
         con = sqlite3.connect(table)
@@ -11,9 +12,11 @@ def df_from_query(table, query, params={}):
 
 class DB:
     def __init__(self, name):
+        # Set database filename with .db extension
         self._dbname = f"./{name}.db"
 
     def load_from_dataframe(self, df, table_name):
+        # Load a DataFrame into a SQLite table (overwrite if it exists)
         con = None
         try:
             con = sqlite3.connect(self._dbname)
@@ -23,9 +26,11 @@ class DB:
                 con.close()
 
     def query(self, q, params={}):
+        # Run a SELECT query and return results as DataFrame
         return df_from_query(self._dbname, q, params=params)
 
     def execute(self, q):
+        # Run a non SELECT query (CREATE/DELETE)
         con = None
         try:
             con = sqlite3.connect(self._dbname)
