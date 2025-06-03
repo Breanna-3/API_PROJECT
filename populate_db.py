745bc64255bf7bbb9c77774bb527d3f1c2e580a7
ammal845 <a.samatar1220231@arts.ac.uk>
2025-06-03 13:41:30 +0100

    Update populate_db.py

--- populate_db.py
+++ populate_db.py
@@ -36,6 +36,7 @@ for page in range(NUM_PAGES):
             'runtime': show.get('runtime'),
             'rating': show['rating']['average'] if show.get('rating') and show['rating']['average'] is not None else None,
             'image': show['image']['medium'] if show.get('image') and show['image'].get('medium') else None
+             'summary': show.get('summary')
         }
 
 # Convert to DataFrame and load into the database
